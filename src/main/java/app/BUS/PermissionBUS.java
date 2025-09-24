a6fd7700c88748ff5ec3a1fcbe9c0fa8eeee1a01
XnCanh <xuanc3879@gmail.com>
2025-09-24 14:47:00 +0700

    Permission and Function. Add PermissionGUI

---
+++ src/main/java/app/BUS/PermissionBUS.java
@@ -0,0 +1,34 @@
+package app.BUS;
+
+import java.util.List;
+
+import app.DAO.PermissionDAO;
+import app.DTO.Permission;
+
+public class PermissionBUS {
+    private PermissionDAO dao;    
+
+    public PermissionBUS() {
+        dao = new PermissionDAO();
+    }
+
+    public List<Permission> getAll() {
+        return dao.getAll();
+    }
+
+    public List<Permission> getRole() {
+        return dao.getRole();
+    }
+
+    public boolean checkPermission(int roleId, int functionId) {
+        return dao.checkPermission(roleId, functionId);
+    }
+
+    public int addPermission(int roleId, int functionId) {
+        return dao.addPermission(roleId, functionId);
+    }
+
+    public int removePermission(int roleId, int functionId) {
+        return dao.removePermission(roleId, functionId);
+    }
+}
---
+++ src/main/java/app/DAO/FunctionDAO.java
@@ -0,0 +1,53 @@
+package app.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.DTO.Function;
+import app.database.DBConnect;
+
+public class FunctionDAO {
+    public List<Function> getAll() {
+        List<Function> list = new ArrayList<>();
+        String sql = "SELECT * FROM chucnang";
+        try (Connection con = DBConnect.getConnection();
+            PreparedStatement st = con.prepareStatement(sql);
+            ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    Function function = new Function (
+                        rs.getInt("idCN"),
+                        rs.getString("TENCN"),
+                        rs.getString("ICON"),
+                        rs.getInt("TRANGTHAI")
+                    );
+                    list.add(function);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        return list;
+    }
+
+    public List<Function> getFunction() {
+        List<Function> list = new ArrayList<>();
+        String sql = "SELECT idCN, TENCN FROM chucnang";
+        try (Connection con = DBConnect.getConnection();
+            PreparedStatement st = con.prepareStatement(sql);
+            ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    Function function = new Function (
+                        rs.getInt("idCN"),
+                        rs.getString("TENCN")
+                    );
+                    list.add(function);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        return list;
+    }
+}
